b54c762c8b9bef356bf536431aebc73c0af6f076
Samuel Ikoli <98848723+samuelIkoli@users.noreply.github.com>
2023-11-06 09:38:17 +0100

    pip mvp done

--- src/Pip.js
+++ src/Pip.js
@@ -11,6 +11,7 @@ function Pip() {
     const [rate, setRate] = useState(0)
     const [to, setTo] = useState('USD')
 
+
     const symbols = [
         'GBPUSD', 'EURJPY',
         'EURUSD', 'EURCHF',
@@ -27,12 +28,6 @@ function Pip() {
 
     const get_rate = async (e) => {
         e.preventDefault()
-
-        console.log('pip is', pip)
-        console.log('lot is', lot)
-        console.log('from is', from)
-        console.log('currency is', to)
-        console.log('12 demo running')
         if (!from || !to || !pip || !lot) {
             return
         }
@@ -41,19 +36,16 @@ function Pip() {
         return rate
     }
     // Time Series FX (Weekly)
-    const alpha_demo = async () => {
-        const res = await fetch('https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=EUR&to_symbol=USD&apikey=demo')
-        const response = await res.json()
-        const rate = response['Time Series FX (Weekly)'][0]
-        console.log(rate)
-    }
+    // const alpha_demo = async () => {
+    //     const res = await fetch('https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=EUR&to_symbol=USD&apikey=demo')
+    //     const response = await res.json()
+    //     const rate = response['Time Series FX (Weekly)'][0]
+    //     console.log(rate)
+    // }
     const twelve_demo = async (from, to) => {
-        // e.preventDefault()
         let ex, res, response
-        console.log('12 demo running')
         const demo_key = '3086f380e87b4353a4fd98f1a2c71b42'
         const url = `https://api.twelvedata.com/time_series?symbol=${from}/${to}&interval=1day&apikey=${demo_key}`
-        console.log(url)
         if (from === to) {
             ex = 1
         }
@@ -61,16 +53,13 @@ function Pip() {
             res = await fetch(url)
             response = await res.json()
             ex = response.values[0].close
-            console.log(response)
         }
         setRate(ex)
-        // console.log('rate is', rate)
         setPipValue(ex * 10 * lot * pip)
-        // return rate
     }
 
-    const alpha_key = `TAOECNDNINUR6ZG6`
-    const alpha_url = `https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=${from}&to_symbol=${to}&apikey=${alpha_key}`
+    // const alpha_key = `TAOECNDNINUR6ZG6`
+    // const alpha_url = `https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=${from}&to_symbol=${to}&apikey=${alpha_key}`
     return (
         <div className='py-5 container justify-content-center'>
             <div className="container justify-content-center gray border rounded-5 border-dark">
@@ -80,7 +69,6 @@ function Pip() {
                         <Form.Label>Symbol</Form.Label>
                         <Form.Select className='border rounded-2 border-secondary' aria-label="Default select example" onChange={(e) => {
                             setFrom(e.target.value.slice(3))
-                            console.log(e.target.value)
                         }}>
                             {symbols.map((symbol, i) => {
                                 return (
